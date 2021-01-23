9532a744af8690d4343e84553a41aee406208935
Jaehyeong Kim <imn00133@gmail.com>
2021-01-24 00:31:12 +0900

    Renamed: PEP8에 따라 name 변경
    BeerNode -> beernode

--- beernode/wsgi.py
+++ beernode/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BeerNode project.
+WSGI config for beernode project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeerNode.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beernode.settings')
 
 application = get_wsgi_application()
