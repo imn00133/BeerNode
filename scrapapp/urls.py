68c18f7e5bbdc1c227f43e3a61f631ed653cf523
jeff8287 <jeff8287@gmail.com>
2021-01-26 20:48:44 +0900

    Added: Create View (scrapapp)
    model의 writer와 date 추후 수정 필요

--- scrapapp/urls.py
+++ scrapapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from scrapapp.views import test
+from scrapapp.views import test, ScrapCreateView
 
 app_name = "scrapapp"
 
 urlpatterns = [
-    path('test/', test, name='test')
+    path('test/', test, name='test'),
+    path('create/', ScrapCreateView.as_view(), name='create'),
 ]
--- scrapapp/views.py
+++ scrapapp/views.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from scrapapp.forms import ScrapCreationForm
+from scrapapp.models import Scrap
+
 
 def test(request):
     return HttpResponse('test message')
+
+class ScrapCreateView(CreateView):
+    model = Scrap
+    form_class = ScrapCreationForm
+    success_url = reverse_lazy('scrapapp:test')
+    template_name = 'scrapapp/create.html'
