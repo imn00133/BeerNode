948b4063c954755571ef630e5b5161947b8f86c8
inhomeENL <lim01000@naver.com>
2021-01-29 13:07:50 +0900

    Added: scraplistapp template 추가
    - magicgrid 비정상작동(display:flex 적용시 세로로 item이 한줄로 나타남) -> 현재 display:inline-block으로 변경
    - list_block: magicgrid 적용으로 블럭형으로 보이는 list / list_oneline: 현재 block으로 내용 한줄씩 나오는 template로 쓸 예정

--- scraplistapp/urls.py
+++ scraplistapp/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 app_name = "scraplistapp"
 
 urlpatterns = [
-    path('scraplist/', TemplateView.as_view(template_name= 'scraplistapp/list.html'), name='scraplist'),
+    path('list/', TemplateView.as_view(
+        template_name= 'scraplistapp/list_oneline.html'), name='scraplist'),
 ]
