6eb44a62350d3defb4ca25a10ccc186e626dd941
inhomeENL <lim01000@naver.com>
2021-01-29 15:03:19 +0900

    Completed: scraplistapp template 초안 완성
    - /scraplist/detail/ 에서 url로 연결된 block형, list형, detail(메인) 이동 가능

--- scraplistapp/urls.py
+++ scraplistapp/urls.py
@@ -5,8 +5,8 @@ app_name = "scraplistapp"
 
 urlpatterns = [
     path('detail/', TemplateView.as_view(
-        template_name= 'scraplistapp/list.html'), name='detail'),
-    path('oneline/', TemplateView.as_view(
+        template_name= 'scraplistapp/list.html'), name='scraplist'),
+    path('list/', TemplateView.as_view(
         template_name='scraplistapp/list_oneline.html'), name='oneline'),
     path('block/', TemplateView.as_view(
         template_name='scraplistapp/list_block.html'), name='block'),
