a9d2b5899834978f27777df995ec363f92adaaa0
inhomeENL <lim01000@naver.com>
2021-01-29 16:37:22 +0900

    Added: accountapp createview
    - /account/create/ 로 접속하면 회원가입 폼이 나옴
    - bootstrap4적용 시도했지만 못함

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
