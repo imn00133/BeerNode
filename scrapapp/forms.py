35bea0920688d59e23849cd952aeb9dcd59673ef
jeff8287 <jeff8287@gmail.com>
2021-02-02 02:17:43 +0900

    Failed: Adding a datepicker
    scrap model에 date를 추가하고 이를 datepicker를 통해 입력받게 하려 했으나
    ScrapCreationForm 내부에 위젯을 정의하고 그걸 create.html에서 한번에 나타내는 방법을 모르겠음..
    class 말고 함수로 하나하나 받아오는 예제는 있는데..

--- scrapapp/forms.py
+++ scrapapp/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 
 from scrapapp.models import Scrap
 
@@ -7,3 +8,10 @@ class ScrapCreationForm(ModelForm):
     class Meta:
         model = Scrap
         fields = ['beer_name', 'place', 'flavor', 'context', 'picture']
+        # date = forms.DateTimeField(
+        #     input_formats=['%d/%m/%Y'],
+        #     widget = forms.DateTimeInput(attrs={
+        #     'class': 'form-control datetimepicker-input',
+        #     'data-target': '#datetimepicker1'
+        # })
+        # )
