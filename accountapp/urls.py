ebd00ac4a61b793bcb24437dcbcfdb3a64b47ac7
inhomeENL <lim01000@naver.com>
2021-02-02 02:19:51 +0900

    Added: accountapp에 detailview 추가
    - test home에 로그인, 가입, 마이페이지 뜨도록 추가
    - 마이페이지 추가에 따른 accountapp detailview 추가

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import AccountCreateView
+from accountapp.views import AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,11 +4,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('scraplistapp:test_home')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
