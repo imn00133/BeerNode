5357328cece5a59b907ecc8ebe338e2437225825
Kim Jaehyeong <imn00133@gmail.com>
2021-02-12 16:49:54 +0900

    BeerNode-55 - Completed: database를 env로 통합
    dev환경의 경우 sqlite3로 정상 접속할 수 있도록 수정

--- beernode/settings.py
+++ beernode/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'beernode.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
+
+if os.environ['PROD'] == 'False':
+    DATABASES['default']['NAME'] = BASE_DIR / 'db.sqlite3'
 
 
 # Password validation
