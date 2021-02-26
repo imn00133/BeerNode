907d09c0260537e68dc5d23e505117b86212d3bc
jeff8287 <jeff8287@gmail.com>
2021-02-27 03:31:57 +0900

    BeerNode-84 Added: accountapp UpdateView 작성
    - 비밀번호 변경 가능
    - profile 관련 변경은 이미 만들어져 있어 따로 만듦

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, DeleteView
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 
 from accountapp.decorators import account_ownership_required
-
+from accountapp.forms import AccountUpdateForm
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -25,6 +25,14 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    context_object_name = 'target_user'
+    template_name = 'accountapp/update.html'
+    success_url = reverse_lazy('test_home')
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
