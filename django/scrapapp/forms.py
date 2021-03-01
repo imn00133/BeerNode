82f5767eb7c02b513a6f868c17f72a117f32b238
lim01000 <lim01000@naver.com>
2021-03-01 23:04:14 +0900

    BeerNode-90 - Added: create에 map 선택 띄우기
    - scraplistapp에 있던 map.html의 내용을 scrap 생성 시 위치 입력란 대신 띄우기 성공
    - 추후 어떻게 변경될지 몰라 getmaplocation.html 파일 남겨두고 include로 map.html을 가져옴
    
    - 오류사항
     - map.html에도 form을 가지고 와서 처리하기 때문에 장소 검색시 검색 결과를 보여주는 대신 create view가 갱신됨
     - 연습시 comment app을 끼워넣은 mixin을 참고하면 해결 가능할 듯
     - map.html에서 결과값인 위치 이름/좌표를 표시하는것 까지는 했지만 이를 scrapapp으로 넘기고 form을 통해 넘기는 것을 구현해야 함

--- django/scrapapp/forms.py
+++ django/scrapapp/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.forms import DateInput
 
 from scrapapp.models import Scrap
+from scrapapp.widgets import GetMapLocation
 
 
 class ScrapCreationForm(ModelForm):
@@ -9,5 +10,6 @@ class ScrapCreationForm(ModelForm):
         model = Scrap
         fields = ['beer_name', 'date', 'place', 'flavor', 'context', 'picture']
         widgets = {
-            'date': DateInput(attrs={'type': 'date'})
+            'date': DateInput(attrs={'type': 'date'}),
+            'place': GetMapLocation(attrs={'type': 'string'})
         }
