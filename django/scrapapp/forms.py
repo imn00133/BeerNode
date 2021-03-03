6adf8f6dea7147af66f433160b254063385bfda7
jeff8287 <jeff8287@gmail.com>
2021-03-03 20:36:49 +0900

    BeerNode-93 Added: Scrap form의 field에 rating 추가, starWidget 지정

--- django/scrapapp/forms.py
+++ django/scrapapp/forms.py
@@ -2,12 +2,13 @@ from django.forms import ModelForm
 from django.forms import DateInput
 
 from scrapapp.models import Scrap
-
+from .widgets import starWidget
 
 class ScrapCreationForm(ModelForm):
     class Meta:
         model = Scrap
-        fields = ['beer_name', 'date', 'place', 'flavor', 'context', 'picture']
+        fields = ['beer_name', 'date', 'place', 'flavor', 'context', 'picture', 'rating']
         widgets = {
-            'date': DateInput(attrs={'type': 'date'})
+            'date': DateInput(attrs={'type': 'date'}),
+            'rating': starWidget,
         }
