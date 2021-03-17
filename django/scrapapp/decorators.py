6d5506006af2f4dd576b286e5d938279ebd3408f
Seungtae Kim <kst20209@naver.com>
2021-03-17 23:45:42 +0900

    Beernode-99 - Completed: 액세스가 거부되는 경우 메인화면으로 redirect
    - decorators.py에서 액세스가 거부되는 경우 HttpResponseForbidden()으로 403을 던지는 대신 render()로 403.html을 던지도록 함.

--- django/scrapapp/decorators.py
+++ django/scrapapp/decorators.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseForbidden
+from django.shortcuts import render
 
 from scrapapp.models import Scrap
 
@@ -7,7 +7,7 @@ def scrap_ownership_required(func):
     def decorated(request, *args, **kwargs):
         scrap = Scrap.objects.get(pk=kwargs['pk'])
         if not scrap.writer == request.user:
-            return HttpResponseForbidden()
+            return render(request, template_name='403.html')
         else:
             return func(request, *args, **kwargs)
     return decorated
