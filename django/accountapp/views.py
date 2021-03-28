9044c9d01a4ab71da14b73af3418f51d7e2f669b
jeff8287 <jeff8287@gmail.com>
2021-03-28 20:01:11 +0900

    BeerNode-162 Added: 계정 생성 후 자동 로그인 구현

--- django/accountapp/views.py
+++ django/accountapp/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -17,6 +19,14 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('test_home')
     template_name = 'accountapp/create.html'
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'],)
+        login(self.request, user)
+        return HttpResponseRedirect(reverse_lazy('test_home'))
 
 
 class AccountDetailView(DetailView):
