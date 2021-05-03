a4521be5897a9cacb6c192cd068476fb4d8ca6d5
Kim Jaehyeong <imn00133@gmail.com>
2021-05-04 00:52:22 +0900

    BeerNode-199 - Added: scrap app get_absolute_url 추가

--- django/scrapapp/models.py
+++ django/scrapapp/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
 from default_image.fields import DefaultStaticImageField
 
 
@@ -21,3 +22,6 @@ class Scrap(models.Model):
     hoppy = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     fruity = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     malty = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def get_absolute_url(self):
+        return reverse('scrapapp:detail', args=[self.pk])
