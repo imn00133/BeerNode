f3833376779005e846e200acfefd725560e14697
jeff8287 <jeff8287@gmail.com>
2021-05-13 17:56:42 +0900

    BeerNode-175 Added: Beerapp - model 작성
    - 모델에 attributes 추가
      - sqlite3에서는 JSONField 사용 불가 -> place1, place2... + brewery는 그냥 CharField로 함
      - Derived attribute를 이제 scrap에서 가져올 방법을 찾아보자
      - detail.html 미작성
    - /admin에서 DB 관리할 수 있도록 수정

--- django/beerapp/admin.py
+++ django/beerapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from beerapp.models import Beer
 
-# Register your models here.
+admin.site.register(Beer)
--- django/beerapp/models.py
+++ django/beerapp/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+from default_image.fields import DefaultStaticImageField
+
 
 class Beer(models.Model):
     beer_name = models.CharField(max_length=128, verbose_name='맥주 이름')
+    rating = models.FloatField(null=True, blank=True)
+    num_scraps = models.IntegerField(default=0)
+    sweet = models.FloatField(null=True, blank=True)
+    malty = models.FloatField(null=True, blank=True)
+    hoppy = models.FloatField(null=True, blank=True)
+    sour = models.FloatField(null=True, blank=True)
+    bitter = models.FloatField(null=True, blank=True)
+    fruity = models.FloatField(null=True, blank=True)
+    place1 = models.CharField(max_length=128, blank=True)
+    place2 = models.CharField(max_length=128, blank=True)
+    image = DefaultStaticImageField(upload_to='beer/', default_image_path='image/no_img.png', null=True, blank=True)
+    body = models.TextField(blank=True)
+    brewery = models.CharField(max_length=128, blank=True)
+    date = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=128, blank=True)
+    genre = models.CharField(max_length=128, blank=True)
+    abv = models.FloatField(null=True, blank=True)
