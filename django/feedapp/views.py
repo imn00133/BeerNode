480f2c5a2ede6a2e0a02bf2a87f22c19ad0edbeb
Seungtae Kim <kst20209@naver.com>
2021-05-18 00:28:16 +0900

    Beernode-178 - Fixed: feedapp/views.py get_context_data에서 get_queryset으로 수정

--- django/feedapp/views.py
+++ django/feedapp/views.py
@@ -12,8 +12,8 @@ class ScrapListView(ListView):
     context_object_name = 'scrap_list'
     template_name = 'listapp/list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['object_list'] = Scrap.objects.filter(writer_id=self.kwargs['pk'])
-        context['filter'] = ScrapFilter(self.request.GET, queryset=context['object_list'])
-        return context
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(writer_id=self.kwargs['pk'])
+        qs = ScrapFilter(self.request.GET, queryset=qs)
+        return qs
