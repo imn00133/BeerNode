0a506696bcf7049ea7fe9deb9a2dae7116aaab19
jeff8287 <jeff8287@gmail.com>
2021-05-23 16:45:33 +0900

    BeerNode-176 Changed : Beerapp model의 attribute 일부 삭제
    rating, num_scraps, sweet, malty, hoppy, sour, bitter, fruity, place1, place2 삭제
    파생된 데이터같은 경우, DB에 attribution으로 두지 않고, 그때그때 처리하는 것으로 한다.
    ex) scrap의 별점을 통해 beer에서 별점 파생)
    추후, DB가 작성되고, 데이터 처리 병목이 생기면 view를 활용하여 처리하는 것으로 변경

--- django/beerapp/models.py
+++ django/beerapp/models.py
@@ -5,16 +5,6 @@ from default_image.fields import DefaultStaticImageField
 
 class Beer(models.Model):
     beer_name = models.CharField(max_length=128, verbose_name='맥주 이름')
-    rating = models.FloatField(null=True, blank=True)
-    num_scraps = models.IntegerField(default=0)
-    sweet = models.FloatField(null=True, blank=True)
-    malty = models.FloatField(null=True, blank=True)
-    hoppy = models.FloatField(null=True, blank=True)
-    sour = models.FloatField(null=True, blank=True)
-    bitter = models.FloatField(null=True, blank=True)
-    fruity = models.FloatField(null=True, blank=True)
-    place1 = models.CharField(max_length=128, blank=True)
-    place2 = models.CharField(max_length=128, blank=True)
     image = DefaultStaticImageField(upload_to='beer/', default_image_path='image/no_img.png', null=True, blank=True)
     body = models.TextField(blank=True)
     brewery = models.CharField(max_length=128, blank=True)
