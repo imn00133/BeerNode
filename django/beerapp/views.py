ea6bf4516d27af7ff7e3ff9dd1dfd6f8ae92c82f
jeff8287 <jeff8287@gmail.com>
2021-05-29 16:32:57 +0900

    tmp

--- django/beerapp/views.py
+++ django/beerapp/views.py
@@ -1,9 +1,19 @@
+from django.db.models import Avg
 from django.views.generic import DetailView
 
 from beerapp.models import Beer
+from scrapapp.models import Scrap
 
 
 class BeerDetailView(DetailView):
     model = Beer
     context_object_name = 'target_beer'
     template_name = 'beerapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        scrap_list = Scrap.objects.filter(beer_name=self.get_object().beer_name)
+        rating = scrap_list.aggregate(Avg('rating'))
+        print(scrap_list)
+        print(rating)
+        return context
